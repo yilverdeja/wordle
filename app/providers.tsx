ad38f9f7aab1bf61d303ab21593da3e155c6d22e
Yil Verdeja <yilverdeja@users.noreply.github.com>
2024-10-14 17:51:29 +0800

    refactor: remove basic game

--- app/providers.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { GameProvider } from "@/context/GameContext";
-
-export default function Providers({ children }: { children: React.ReactNode }) {
-	return <GameProvider>{children}</GameProvider>;
-}
