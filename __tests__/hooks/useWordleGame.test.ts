4d77fe3e71779cc03fc3f7d8a5eb2e1a2722bc59
Yil Verdeja <yilverdeja@users.noreply.github.com>
2024-10-17 17:38:58 +0800

    feat: add guess mock handler

--- __tests__/hooks/useWordleGame.test.ts
+++ __tests__/hooks/useWordleGame.test.ts
@@ -1,5 +1,6 @@
 import { renderHook, act, waitFor } from "@testing-library/react";
 import useWordleGame from "@/hooks/useWordleGame";
+import { LetterMatch } from "@/lib/wordle/types";
 
 describe("useWordleGame", () => {
 	it("should get initial game status", async () => {
@@ -16,6 +17,18 @@ describe("useWordleGame", () => {
 		expect(result.current.guessResults).toHaveLength(0);
 		expect(result.current.rounds).toBe(0);
 		expect(result.current.answer).toBe(null);
+	});
+
+	it("should not allow a guess to be made if the game hasn't started", async () => {
+		const { result } = renderHook(() => useWordleGame());
+
+		act(() => {
+			result.current.submitGuess("");
+		});
+
+		await waitFor(() => expect(result.current.error).not.toBeNull());
+
+		expect(result.current.error?.type).toBe("guess");
 	});
 
 	it("should start a normal game", async () => {
@@ -44,5 +57,50 @@ describe("useWordleGame", () => {
 		await waitFor(() => expect(result.current.error).not.toBeNull());
 
 		expect(result.current.error?.type).toBe("start");
+	});
+
+	it("should handle an invalid guess", async () => {
+		const { result } = renderHook(() => useWordleGame());
+
+		act(() => {
+			result.current.submitGuess("error");
+		});
+
+		await waitFor(() => expect(result.current.error).not.toBeNull());
+
+		expect(result.current.error?.type).toBe("guess");
+		expect(result.current.rounds).toBe(0);
+	});
+
+	it("should handle an incorrect guess", async () => {
+		const { result } = renderHook(() => useWordleGame());
+
+		act(() => {
+			result.current.submitGuess("");
+		});
+
+		await waitFor(() =>
+			expect(result.current.guessResults).toHaveLength(1)
+		);
+
+		expect(result.current.guessResults[0]).toHaveProperty("guess", "");
+		expect(result.current.guessResults[0]).toHaveProperty("result", [
+			LetterMatch.Hit,
+			LetterMatch.Miss,
+			LetterMatch.Present,
+			LetterMatch.Miss,
+			LetterMatch.Hit,
+		]);
+		expect(result.current.rounds).toBe(1);
+	});
+
+	it("should win on the right guess", async () => {
+		const { result } = renderHook(() => useWordleGame());
+
+		act(() => {
+			result.current.submitGuess("right");
+		});
+
+		await waitFor(() => expect(result.current.status).toEqual("win"));
 	});
 });
--- __tests__/msw/handlers.ts
+++ __tests__/msw/handlers.ts
@@ -1,43 +1,139 @@
+import WordleGame from "@/lib/wordle/WordleGame";
 import { http, HttpResponse } from "msw";
 
-let gameStatus = "none";
-let currentGameType = "normal";
+const session: WordleGame = new WordleGame(10);
 
 export const handlers = [
 	http.get("/api/game", () => {
 		return HttpResponse.json({
-			status: gameStatus,
-			type: "normal",
-			results: [],
-			maxTries: 10,
-			tries: 0,
-			answer: "",
+			status: session.status,
+			type: session.type,
+			results: session.results,
+			maxTries: session.maxNumTries,
+			tries: session.tries,
+			answer: session.answer,
 		});
 	}),
 	http.post("/api/game/start", async ({ request }) => {
 		const data = await request.json();
 		const gameType = data.gameType;
 
-		if (gameStatus === "pending") {
+		if (session.status === "pending") {
 			return HttpResponse.json(
 				{
-					status: gameStatus,
-					type: currentGameType,
-					maxTries: 10,
-					tries: 0,
+					status: session.status,
+					type: session.type,
+					maxTries: session.maxNumTries,
+					tries: session.tries,
 					error: "A game is already active",
 				},
 				{ status: 409 }
 			);
 		}
 
-		gameStatus = "pending";
-		currentGameType = gameType;
+		session.status = "pending";
+		session.type = gameType;
+		if (session.type === "absurdle") {
+			session.candidates = "0,1,2,3,4,5";
+			session.answer = "";
+		} else {
+			session.candidates = "";
+			session.answer = "right";
+		}
+
+		session.tries = 0;
+		session.results = [];
 
 		return HttpResponse.json({
-			status: gameStatus,
-			type: currentGameType,
-			maxTries: 10,
+			status: session.status,
+			type: session.type,
+			maxTries: session.maxNumTries,
+		});
+	}),
+	http.post("/api/game/guess", async ({ request }) => {
+		const data = await request.json();
+		const guess = data.guess;
+
+		// handle a game that hasn't started
+		if (session.status !== "pending") {
+			return HttpResponse.json(
+				{
+					status: session.status,
+					error: "The game has not started yet",
+				},
+				{ status: 400 }
+			);
+		}
+
+		// handle an invalid guess input
+		if (guess === "error") {
+			return HttpResponse.json(
+				{
+					status: session.status,
+					error: "Invalid guess",
+				},
+				{ status: 403 }
+			);
+		}
+
+		// handle guess when game is pending
+		session.tries += 1;
+
+		// handle absurdle with no answer chosen
+		if (session.type === "absurdle" && session.answer === "") {
+			if (session.candidates === "0") {
+				session.answer = "right";
+				session.candidates = "";
+			} else {
+				session.candidates = session.candidates
+					.split(",")
+					.slice(0, -1)
+					.join(",");
+				session.results.push({ guess, result: "MMMMM" });
+
+				if (session.maxNumTries === session.tries) {
+					session.status = "lost";
+				}
+
+				return HttpResponse.json({
+					status: session.status,
+					type: session.type,
+					tries: session.tries,
+					results: session.results,
+					guess,
+					answer: null,
+				});
+			}
+		}
+
+		// handle when answer has been chosen
+		if (guess === "right") {
+			session.status = "win";
+			session.results.push({ guess, result: "HHHHH" });
+
+			return HttpResponse.json({
+				status: session.status,
+				type: session.type,
+				tries: session.tries,
+				results: session.results,
+				guess,
+				answer: session.answer,
+			});
+		}
+
+		if (session.maxNumTries === session.tries) {
+			session.status = "lost";
+		}
+
+		session.results.push({ guess, result: "HMPMH" });
+
+		return HttpResponse.json({
+			status: session.status,
+			type: session.type,
+			tries: session.tries,
+			results: session.results,
+			guess,
+			answer: session.status === "lost" ? session.answer : null,
 		});
 	}),
 ];
